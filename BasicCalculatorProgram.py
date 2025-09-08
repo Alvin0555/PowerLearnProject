8680e60190ef3ffd682f4e20e70516ec5918aa0b
Alvin0555 <agnymda@outlook.com>
2025-09-07 22:18:36 -0400

    First commit

--- BasicCalculatorProgram.py
+++ BasicCalculatorProgram.py
@@ -1,3 +1,34 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Impossible, division by zero"
+    else:
+        return a / b
+    
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide }
+
+first_number = int(input("Enter the first number : "))
+second_number = int(input("Enter the second number : "))
+operation = input("Enter the operation (+, -, *, /) : ")
+
+result = operations[operation](first_number, second_number)
+
+print("The result of ", first_number, operation, second_number, " is ", result)
+
+
+"""
 a = int(input("First number : "))
 b = int(input("Second number : "))
 operation = input("Mathematique opération (+, -, * or /) : ")
@@ -13,3 +44,4 @@ if operation == '*':
     
 if operation == '/':
     print(a, " / ", b, " = ", a/b)
+"""
